2a2ecf23cf74351d892aba4a2ca754fbab72d0b2
Matas_Pieczulis <51287415+Sekizyo@users.noreply.github.com>
2024-04-25 08:12:27 +0200

    Time: 110 ms (61.58%) | Memory: 19.1 MB (33.94%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,11 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        output = nums[0]
+        count = {}
+        for num in nums:
+            if num not in count.keys():
+                count[num] = 1
+            else:
+                count[num] += 1
+        return min(count, key=count.get)
+        
