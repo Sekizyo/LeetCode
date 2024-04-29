81aaaf87b8f18b1765779842fd8a9cb1e4bb7835
Matas_Pieczulis <51287415+Sekizyo@users.noreply.github.com>
2024-04-29 13:38:14 +0200

    Time: 2352 ms (14.00%) | Memory: 17 MB (5.25%) - LeetSync

--- 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/factorial-trailing-zeroes.py
@@ -1,17 +1,16 @@
 import sys
+import math
 sys.set_int_max_str_digits(0)
 
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        m = 1
-        for i in range(1, n+1):
-            m *= i
-
+        m = math.factorial(n)
         count = 0
-        mList = str(m)
+        mList = list('%d' % m)
 
         for i in range(len(mList)-1, 0, -1):
-            if mList[i] == '0':
+            char = mList[i]
+            if char == '0':
                 count += 1
             else:
                 break
