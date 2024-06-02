623c212e3756c4fb6ec430043473da93b3c5ecc6
Matas_Pieczulis <51287415+Sekizyo@users.noreply.github.com>
2024-06-02 12:45:38 +0200

    Time: 834 ms (25.90%) | Memory: 16.3 MB (99.66%) - LeetSync

--- 2998-count-symmetric-integers/count-symmetric-integers.py
+++ 2998-count-symmetric-integers/count-symmetric-integers.py
@@ -4,19 +4,9 @@ class Solution:
         for num in range(low, high+1):
             if len(str(num)) % 2 == 0:
                 num = str(num)
-
-                left  = list(num[:len(num)//2])
-                right = list(num[len(num)//2:])
-
-                leftF = []
-                rightF = []
-                for num1 in left:
-                    leftF.append(int(num1))
-                
-                for num2 in right:
-                    rightF.append(int(num2))
-
-                if sum(leftF) == sum(rightF):
+                left = [int(i) for i in num[:len(num)//2]]
+                right = [int(i) for i in num[len(num)//2:]]
+                if sum(left) == sum(right):
                     count += 1
 
         return count
