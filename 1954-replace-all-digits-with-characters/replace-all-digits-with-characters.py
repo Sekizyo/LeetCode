2e7cf66fe7b3f797b42b59c62f936d123396bade
Matas_Pieczulis <51287415+Sekizyo@users.noreply.github.com>
2024-06-03 15:17:57 +0200

    Time: 31 ms (82.12%) | Memory: 16.7 MB (9.12%) - LeetSync

--- 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.py
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.py
@@ -3,9 +3,8 @@ class Solution:
         output = ""
         for i in range(0, len(s)-1, 2):
             letter = s[i]
-            num = s[i+1]
             output += letter
-            output += chr(ord(letter) + int(num))
+            output += chr(ord(letter) + int(s[i+1]))
         
         if (len(s)-1) % 2 == 0:
             output += s[-1]
